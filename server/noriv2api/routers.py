f8cb4c2de6344763c6c0cd760d7c77c871bf877e
Moritz Schäfer <mail@moritzs.de>
2015-12-11 06:09:37 +0800

    add test ping verb

--- server/noriv2api/routers.py
+++ server/noriv2api/routers.py
@@ -5,11 +5,14 @@ from swampdragon.route_handler import BaseRouter
 class UpdateMsgRouter(BaseRouter):
     route_name = 'update-msg'
     valid_verbs = [
-        'subscribe', 'unsubscribe'
+        'subscribe', 'unsubscribe', 'ping'
     ]
 
     def subscribe(self, **kwargs):
         import ipdb; ipdb.set_trace() # channel has to be user-{id}
         super().subscribe(**kwargs)
+
+    def ping(self, **kwargs):
+        self.send({'pong': 'pong'})
 
 route_handler.register(UpdateMsgRouter)
